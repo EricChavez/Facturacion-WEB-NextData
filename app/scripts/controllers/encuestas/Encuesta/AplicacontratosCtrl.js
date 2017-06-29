42b53136a0cf4a395c638de9d1e40162c8e5138f
EricChavez <erictics@gmail.com>
2017-06-29 17:38:26 -0500

    pantallas de proceso encuestas

---
+++ app/scripts/controllers/encuestas/Encuesta/AplicacontratosCtrl.js
@@ -0,0 +1,15 @@
+'use strict';
+angular
+  .module('softvApp')
+  .controller('AplicacontratosCtrl', function ($uibModal, $rootScope, ngNotify, encuestasFactory) {
+
+    function initialData() {
+     
+    }
+
+    
+
+    var vm = this;
+    initialData();
+  
+  });
