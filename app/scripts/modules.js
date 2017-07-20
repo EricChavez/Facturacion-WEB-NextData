8d5158b8b83e20096f03a12d714bf408bc571dc8
EricChavez <erictics@gmail.com>
2017-07-20 15:15:40 -0500

    cambios en encuestas

--- app/scripts/modules.js
+++ app/scripts/modules.js
@@ -5,7 +5,7 @@ angular
 		var states = [{
 				name: 'home',
 				data: {
-					pageTitle: 'BIENVENIDO | SAC WEB',
+					pageTitle: 'BIENVENIDO | softv WEB',
 					permissions: {
 						except: ['anonymous'],
 						options: {
@@ -26,7 +26,7 @@ angular
 				name: 'auth',
 				url: '/auth/?token',
 				data: {
-					pageTitle: 'BIENVENIDO | SAC WEB'
+					pageTitle: 'BIENVENIDO | softv WEB'
 				},
 				views: {
 					'loginview': {
