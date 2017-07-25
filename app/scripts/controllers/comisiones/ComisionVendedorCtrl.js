ebbb28fd0cf8be72d38277f2d84a09effd00b5db
alesabas <alejandro_jimenez24@hotmail.com>
2017-07-25 17:33:07 -0500

    Comision Vendedor

--- app/scripts/controllers/comisiones/ComisionVendedorCtrl.js
+++ app/scripts/controllers/comisiones/ComisionVendedorCtrl.js
@@ -2,6 +2,72 @@
 
 angular
     .module('softvApp')
-    .controller('ComisionVendedorCtrl', function () {
+    .controller('ComisionVendedorCtrl', function (ComisionFactory, atencionFactory, ngNotify, $uibModal, $state) {
+
+        function InitialData(){
+            ComisionFactory.GetComisionesVendedoresWebList().then(function(data){
+                vm.ComisionVLista = data.GetComisionesVendedoresWebListResult;
+                if (vm.ComisionVLista.length == 0) {
+				    vm.sinRegistros = true;
+					vm.conRegistros = false;
+				} else {
+				    vm.sinRegistros = false;
+					vm.conRegistros = true;
+				}
+            });
+            atencionFactory.serviciosNuevo().then(function(data){
+                vm.TipoServiciosLista = data.GetMuestraTipSerPrincipal2ListResult;
+            });
+        }
+
+        function BuscarServicio(){
+            ComisionFactory.GetServiciosWebList(vm.TipoServico.Clv_TipSerPrincipal).then(function(data){
+                vm.ServiciosLista = data.GetServiciosWebListResult;
+            });
+        }
+
+        function AgregarComisionVendedor(){
+            if(vm.RangoFinal > vm.RangoInicial && vm.RangoInicial > 0 && vm.RangoFinal > 0){
+                if(vm.Comision > 0){
+                    ComisionFactory.AddComisionesVendedoresWeb(vm.TipoServico.Clv_TipSerPrincipal, vm.Servicio.Clv_Servicio, vm.RangoInicial, vm.RangoFinal, vm.Comision).then(function(data){
+                        ngNotify.set('Se ha guardado la comisión con éxito','success');
+                        $state.reload('home.comisiones.vendedores');
+                    });
+                }else{
+                     ngNotify.set('Ingresa una comisión válida','warn'); 
+                }
+            }else{
+               ngNotify.set('El rango inicial no puede ser mayor o igual al rango final','warn');
+            }
+        }
+
+        function ConfirmarEliminar(IdComision){
+            var options = {};
+            options.IdComision = IdComision;
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'views/comisiones/ModalEliminarComVen.html',
+                controller: 'ModalEliminarComVenCtrl',
+                controllerAs: 'ctrl',
+                backdrop: 'static',
+                keyboard: false,
+                class: 'modal-backdrop fade',
+                size: 'sm',
+                resolve: {
+                    options: function () {
+                        return options;
+                    }
+                }
+            });
+        }
+
         var vm = this;
+
+        vm.AgregarComisionVendedor = AgregarComisionVendedor;
+        vm.BuscarServicio = BuscarServicio;
+        vm.ConfirmarEliminar = ConfirmarEliminar;
+        
+        InitialData();
     });
--- app/scripts/factories/ComisionFactory.js
+++ app/scripts/factories/ComisionFactory.js
@@ -5,9 +5,11 @@ angular.module('softvApp')
     var paths = {
       GetcomisiontecnicoList: '/ComisionesTecnicosWeb/GetComisionesTecnicosWebList',
       Addcomisiontecnico: '/ComisionesTecnicosWeb/AddComisionesTecnicosWeb',
-     
-    };   
-
+      GetComisionesVendedoresWebList: '/ComisionesVendedoresWeb/GetComisionesVendedoresWebList',
+      AddComisionesVendedoresWeb: '/ComisionesVendedoresWeb/AddComisionesVendedoresWeb',
+      GetServiciosWebList: '/ServiciosWeb/GetServiciosWebList',
+      DeleteComisionesVendedoresWeb: '/ComisionesVendedoresWeb/DeleteComisionesVendedoresWeb'
+    }; 
 
      factory.GetcomisiontecnicoList = function () {
       var deferred = $q.defer();
@@ -55,8 +57,82 @@ angular.module('softvApp')
 
     };
 
+    factory.GetComisionesVendedoresWebList = function () {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      $http.get(globalService.getUrl() + paths.GetComisionesVendedoresWebList, config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
 
-    
+    factory.GetServiciosWebList = function (ClvTipSer) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var parametros = {
+                'ClvTipSer': ClvTipSer
+            };
+      $http.post(globalService.getUrl() + paths.GetServiciosWebList, JSON.stringify(parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    };
+
+    factory.AddComisionesVendedoresWeb = function (Clv_Tipservicios, Clv_Servicio, RangoInicial, RangoFinal, Comsion) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var parametros = {
+        'objComisionesVendedoresWeb': {
+          'Clv_Tipservicios': Clv_Tipservicios,
+          'Clv_Servicio': Clv_Servicio,
+          'RangoInicial': RangoInicial,
+          'RangoFinal': RangoFinal,
+          'Comsion': Comsion
+        }
+      };
+      $http.post(globalService.getUrl() + paths.AddComisionesVendedoresWeb, JSON.stringify(parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
+    factory.DeleteComisionesVendedoresWeb = function (IdComision) {
+      var deferred = $q.defer();
+      var config = {
+        headers: {
+          'Authorization': $localStorage.currentUser.token
+        }
+      };
+      var parametros = {
+        'IdComision': IdComision
+      };
+      console.log(parametros);
+      $http.post(globalService.getUrl() + paths.DeleteComisionesVendedoresWeb, JSON.stringify(parametros), config).then(function (response) {
+        deferred.resolve(response.data);
+      }).catch(function (response) {
+        deferred.reject(response);
+      });
+      return deferred.promise;
+    };
+
     return factory;
 
 
